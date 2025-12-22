a3b25bd16953b64309d41a8942825f923efa62f2
saisrivathsan9 <58337977+saisrivathsan9@users.noreply.github.com>
2025-12-21 20:27:14 -0700

    Fixed bugs v1

---
+++ Diary/AttachementPreview.swift
@@ -0,0 +1,8 @@
+//
+//  AttachementPreview.swift
+//  Diary
+//
+//  Created by Saisrivathsan Manikandan on 12/21/25.
+//
+
+import Foundation
---
+++ Diary/TicketFormView.swift
@@ -0,0 +1,8 @@
+//
+//  TicketFormView.swift
+//  Diary
+//
+//  Created by Saisrivathsan Manikandan on 12/21/25.
+//
+
+import Foundation
